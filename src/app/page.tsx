1beba9bc3c0f3481184ec333f98fe30d42fafdf2
ahmedjoseph07 <ahmedjoseph11@gmail.com>
2025-02-10 21:15:47 +0600

    hi removed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
        <Button >
-        hi hi
+        hi
        </Button>
     </main>
   )
