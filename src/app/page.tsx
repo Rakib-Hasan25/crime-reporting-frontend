8ef46a9d3d4541754c73fda376e78884cd2d8aa5
Anawrulkabir <mdanawrulkabirfahad123@gmail.com>
2025-02-10 21:30:39 +0600

    added hello

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,9 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-
       <Button>hi hi</Button>
       <Button>Hi from fahad</Button>
-
-     
+      <p>hello</p>
     </main>
   )
 }
